d5782aba6bdeb46e2e96ad9aba94a24dc043d572
jordiascension <jordiascension@enterpriseconsulting.es>
2024-04-22 01:06:33 +0200

    First Commit with crud implementation and server rendering in .Net Core 8.0

---
+++ Data/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using static System.Net.WebRequestMethods;
+
+namespace InprocessWebSite.Data
+{
+    public interface IMovieService
+    {
+        public Task<Movie[]> GetMoviesAsync();
+        public Task<Movie?> GetMovieByIdAsync(int id);
+        public Task<Movie?> AddMovieAsync(Movie movie);
+        public Task<Movie?> UpdateMovieAsync(Movie movie);
+        public Task DeleteMovieAsync(int id);
+    }
+}
---
+++ Data/MovieService.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Text.Json;
+
+using static System.Net.WebRequestMethods;
+
+namespace InprocessWebSite.Data
+{
+    public class MovieService : IMovieService
+    {
+        HttpClient _httpClient;
+
+        public MovieService(HttpClient httpClient) {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Movie?> AddMovieAsync(Movie movie)
+        {
+            var response = await _httpClient.PostAsJsonAsync("", movie);
+            var movieInserted = await response.Content.ReadFromJsonAsync<Movie>();
+            return movieInserted;
+
+        }
+
+        public async Task DeleteMovieAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync(id.ToString());   
+        }
+
+        public async Task<Movie?> GetMovieByIdAsync(int id)
+        {
+            Movie? movie = await _httpClient.GetFromJsonAsync<Movie>("" + id);
+            return movie;
+        }
+
+        public async Task<Movie[]> GetMoviesAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<Movie[]>("") ?? [];
+        }
+
+        public async Task<Movie?> UpdateMovieAsync(Movie movie)
+        {
+            var response = await _httpClient.PutAsJsonAsync("", movie);
+            //var movieUpdated = await response.Content.ReadFromJsonAsync<Movie>();
+            return new Movie();
+        }
+    }
+}
